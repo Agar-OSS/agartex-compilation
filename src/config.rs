7f35820c86669e6e2d85ddceb65816583538b0d4
Tomasz Mazur <mazur.tomasz34@gmail.com>
2023-04-22 22:29:11 +0200

    Requested changes

--- src/config.rs
+++ src/config.rs
@@ -12,6 +12,28 @@ pub struct Config {
 
 const CONFIG_PATH: &str = "config.json";
 
+pub const SERVER_ADDR_ENV_VAR: &str = "SERVER_ADDR";
+pub const TMP_FILE_DIR_ENV_VAR: &str = "TMP_FILE_DIR";
+pub const LATEXMK_PATH_ENV_VAR: &str = "LATEXMK_PATH";
+
+fn load_config() -> Config {
+    let mut cfg: Config = serde_json::from_reader(BufReader::new(File::open(Path::new(CONFIG_PATH)).unwrap())).unwrap();
+
+    if let Ok(server_addr) = std::env::var(SERVER_ADDR_ENV_VAR) {
+        cfg.server_addr = server_addr.parse().unwrap();
+    }
+
+    if let Ok(tmp_file_dir) = std::env::var(TMP_FILE_DIR_ENV_VAR) {
+        cfg.tmp_file_dir = tmp_file_dir.parse().unwrap();
+    }
+
+    if let Ok(latexmk_path) = std::env::var(LATEXMK_PATH_ENV_VAR) {
+        cfg.latexmk_path = latexmk_path.parse().unwrap();
+    }
+
+    cfg
+}
+
 lazy_static! {
-    pub static ref CONFIG: Config = serde_json::from_reader(BufReader::new(File::open(Path::new(CONFIG_PATH)).unwrap())).unwrap();
+    pub static ref CONFIG: Config = load_config();
 }
