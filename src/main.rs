57f8d580fc10e136c3a39b8fc148b1086dc920d5
Tomasz Mazur <mazur.tomasz34@gmail.com>
2023-06-16 00:06:05 +0200

    Create temporary file dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use tokio::fs;
 use tracing::{error, info};
 
 mod constants;
@@ -13,9 +14,12 @@ use routing::main_router;
 
 use constants::SERVER_URL;
 
+use crate::constants::TMP_FILE_DIR;
+
 #[tracing::instrument]
 pub async fn run() -> anyhow::Result<()> {
     tracing_subscriber::fmt().init();
+    fs::create_dir_all(TMP_FILE_DIR.as_path()).await.unwrap();
 
     info!("Running server!");
     axum::Server::try_bind(&SERVER_URL)?
